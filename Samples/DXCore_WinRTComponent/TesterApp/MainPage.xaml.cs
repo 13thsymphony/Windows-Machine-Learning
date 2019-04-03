c9a2f77a039c4cbcb075d2529ff217db6f5ab399
Simon Tao <sitao@microsoft.com>
2019-04-03 22:28:49 +0000

    Merged PR 10: New sample: DXCore WinRT helper
    This sample allows WinML UWP apps to use MCDM without writing C++/DXCore code by implementing a WinRT Component. Ryan's squeezenet sample shows how to use DXCore for the C++ level developer; this sample is the most convenient method for C# and other UWP apps.
    
    - This sample will become irrelevant when the final Vb WinRT APIs are built. But it will be one of the first samples to show how to use some of the DXCore features so I want to make sure it is consistent with our recommendations.
    - I want to ship this as part of the AI PC dev kit. Intel will also use this sample to help with internal demo writing.
    - Minimal test app that just gets the LMDevice and doesn't do anything with it.
    
    Testing:
    -  Tested on a MyriadX NUC with 18870

---
+++ Samples/DXCore_WinRTComponent/TesterApp/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.AI.MachineLearning;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TesterApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonRun_Click(object sender, RoutedEventArgs e)
+        {
+            var helper = new DXCore_WinRTComponent.DXCoreHelper();
+
+            // Graphics-capable adapter
+
+            LearningModelDevice gfxLMDevice = helper.GetDeviceFromGraphicsAdapter();
+
+            TextOutput.Text += "Creating a WinML LearningModelDevice from the first valid graphics-capable adapter (e.g. a GPU): ";
+
+            if (gfxLMDevice != null)
+            {
+                TextOutput.Text += "Successful (Adapter ID " + gfxLMDevice.AdapterId.HighPart + "-" + gfxLMDevice.AdapterId.LowPart + ")\n";
+            }
+            else
+            {
+                TextOutput.Text += "Unsuccessful (no valid adapter found)\n";
+            }
+
+            // Compute-only adapter
+
+            LearningModelDevice compLMDevice = helper.GetDeviceFromComputeOnlyAdapter();
+
+            TextOutput.Text += "Creating a WinML LearningModelDevice from the first valid compute-only adapter (e.g. a VPU): ";
+
+            if (compLMDevice != null)
+            {
+                TextOutput.Text += "Successful (Adapter ID " + compLMDevice.AdapterId.HighPart + "-" + compLMDevice.AdapterId.LowPart + ")\n";
+            }
+            else
+            {
+                TextOutput.Text += "Unsuccessful (no valid adapter found)\n";
+            }
+
+            // Intel® Movidius™ VPU
+
+            LearningModelDevice vpuLMDevice = helper.GetDeviceFromVpuAdapter();
+
+            TextOutput.Text += "Creating a WinML LearningModelDevice hardcoded to select the Intel® Movidius™ VPU: ";
+
+            if (vpuLMDevice != null)
+            {
+                TextOutput.Text += "Successful (Adapter ID " + vpuLMDevice.AdapterId.HighPart + "-" + vpuLMDevice.AdapterId.LowPart + ")\n";
+            }
+            else
+            {
+                TextOutput.Text += "Unsuccessful (no valid adapter found)\n";
+            }
+        }
+    }
+}
